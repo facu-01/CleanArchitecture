619e64947785cd8007308934c99ba5f34a9d0f89
Facundo Lucero <facundon.lucero@alumnos.frm.utn.edu.ar>
2025-10-12 12:17:14 -0300

    fix: nuget versions

---
+++ src/CleanArchitecture.Api/Program.cs
@@ -0,0 +1,28 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Infraestructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddInfraestructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapControllers();
+
+
+app.Run();
+
